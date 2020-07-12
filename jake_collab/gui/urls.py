5fab25710d53752c9899bff031f85af769ee4a86
NoahLE <noah@noahle.com>
2020-07-12 14:45:37 -0500

    Base commit

---
+++ jake_collab/gui/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('gui/login', views.login),
+    path('gui/home', views.home),
+    path('gui/wiki', views.wiki),
+    path('gui/article', views.article),
+    path('gui/kanban', views.kanban),
+    path('gui/profile-public', views.profile_public),
+    path('gui/profile-private', views.profile_private),
+    path('gui/walkthrough', views.walkthrough),
+    path('gui/settings', views.settings),
+    path('gui/notifications', views.notifications),
+    path('gui/messaging', views.messaging),
+    path('gui/calendar', views.calendar),
+    path('gui/chat-room', views.chat_room)
+    # path('gui/login', views.login),
+    # path('gui/login', views.login),
+    # path('gui/login', views.login),
+    # path('gui/login', views.login),
+    # path('gui/login', views.login),
+    # path('gui/login', views.login),
+    # path('gui/login', views.login)
+]
